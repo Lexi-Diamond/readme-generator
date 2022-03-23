7a04feebb07283bed16114a1c828c9e14d0cd31f
Lexi Dorrity <lexidorrity@Lexis-MBP.socal.rr.com>
2022-03-22 20:23:34 -0700

    started generateMarkdown function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,9 +12,16 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  return `# ${data.name}
+          # ${data.github}
+          # ${data.title}
+          # ${data.license}
+          # ${data.description}
+          # ${data.installation}
+          # ${data.usage}
+          # ${data.contributing}
+          # ${data.tests}`
+;
 }
 
 module.exports = generateMarkdown;
